e7a77a870d0e0a1242112918ad0485ff12a6cba2
Chauhan-1999 <tarunchauhan798528@gmail.com>
2026-01-17 23:52:55 +0530

    adding simple producer[user-service]-consumer[notification-service]

---
+++ learnKafka/user-service/src/main/java/com/chauhan/learnKafka/user_service/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.chauhan.learnKafka.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @Value("${kafka.topic.user-random-topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/{message}")
+    public ResponseEntity<String> sendMessage(@PathVariable String message) {//producer
+
+        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC,message);
+        return ResponseEntity.ok("Message queued");
+    }
+
+}
